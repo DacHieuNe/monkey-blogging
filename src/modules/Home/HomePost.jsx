4d19840afb302d9d6ff41151291d581fe00ee319
Hieudz <checkerpssss@gmail.com>
2023-05-26 11:49:03 +0700

    Upload source code 26/5/2023 11:48AM

--- src/modules/Home/HomePost.jsx
+++ src/modules/Home/HomePost.jsx
@@ -15,50 +15,66 @@ const homePost = [
   {
     id: Math.floor(Math.random() * 10000000 + 1),
     title: "Hướng dẫn setup phòng cực chill dành cho người mới toàn tập",
-    tag: "Kiến thức",
+    category: {
+      name: "Kiến thức",
+      slug: "kien-thuc",
+    },
     users: {
       fullname: "Andiez Le",
     },
     createdAt: {
       seconds: new Date().getTime(),
     },
-    bg: "https://i.pinimg.com/736x/80/2b/94/802b946554f7a4f2a03c69012013adb4.jpg",
+    image_link:
+      "https://i.pinimg.com/736x/80/2b/94/802b946554f7a4f2a03c69012013adb4.jpg",
   },
   {
     id: Math.floor(Math.random() * 10000000 + 1),
     title: "Hướng dẫn setup phòng cực chill dành cho người mới toàn tập",
-    tag: "Kiến thức",
+    category: {
+      name: "Kiến thức",
+      slug: "kien-thuc",
+    },
     users: {
       fullname: "Andiez Le",
     },
     createdAt: {
       seconds: new Date().getTime(),
     },
-    bg: "https://i.pinimg.com/736x/80/2b/94/802b946554f7a4f2a03c69012013adb4.jpg",
+    image_link:
+      "https://i.pinimg.com/736x/80/2b/94/802b946554f7a4f2a03c69012013adb4.jpg",
   },
   {
     id: Math.floor(Math.random() * 10000000 + 1),
     title: "Hướng dẫn setup phòng cực chill dành cho người mới toàn tập",
-    tag: "Kiến thức",
+    category: {
+      name: "Kiến thức",
+      slug: "kien-thuc",
+    },
     users: {
       fullname: "Andiez Le",
     },
     createdAt: {
       seconds: new Date().getTime(),
     },
-    bg: "https://i.pinimg.com/736x/80/2b/94/802b946554f7a4f2a03c69012013adb4.jpg",
+    image_link:
+      "https://i.pinimg.com/736x/80/2b/94/802b946554f7a4f2a03c69012013adb4.jpg",
   },
   {
     id: Math.floor(Math.random() * 10000000 + 1),
     title: "Hướng dẫn setup phòng cực chill dành cho người mới toàn tập",
-    tag: "Kiến thức",
+    category: {
+      name: "Kiến thức",
+      slug: "kien-thuc",
+    },
     users: {
       fullname: "Andiez Le",
     },
     createdAt: {
       seconds: new Date().getTime(),
     },
-    bg: "https://i.pinimg.com/736x/80/2b/94/802b946554f7a4f2a03c69012013adb4.jpg",
+    image_link:
+      "https://i.pinimg.com/736x/80/2b/94/802b946554f7a4f2a03c69012013adb4.jpg",
   },
 ];
 const HomePost = (props) => {
@@ -68,7 +84,9 @@ const HomePost = (props) => {
         <div className="row">
           {Array.isArray(homePost) &&
             homePost.length > 0 &&
-            homePost.map((item) => <HomePostItem width="primary" item={item} key={item.id} />)}
+            homePost.map((item) => (
+              <HomePostItem width="primary" item={item} key={item.id} />
+            ))}
         </div>
       </div>
     </HomePostStyle>
